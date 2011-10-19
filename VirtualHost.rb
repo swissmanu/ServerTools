51dee2fc37286c4019083bae0a4492e5053c1f5a
Manuel Alabor <msites@msites.net>
2011-10-19 07:54:32 +0200

    added configuration parsing

--- VirtualHost.rb
+++ VirtualHost.rb
@@ -9,14 +9,14 @@ def parse_arguments()
   arguments = {}
 
   optparse = OptionParser.new do |opts|
-    opts.banner = "Use: AddVirtualHost.rb --name [virtual host name] --dns [dns name]"
+    opts.banner = "Use: VirtualHost.rb --add --name VHOSTNAME"
   
+    opts.on('--add', 'Adds a new VirtualHost to Apache') do
+      arguments[:action] = :add
+    end
+    
     opts.on('--name NAME', String, 'Name for the VirtualHost') do |name|
       arguments[:name] = name
-    end
-  
-    opts.on('--dns DNS', String, 'DNS name which should be mapped to this VirtualHost') do |dns|
-      arguments[:dns] = dns
     end
   
     opts.on('-h', '--help', 'Display this screen') do
@@ -26,12 +26,29 @@ def parse_arguments()
   end
 
   optparse.parse!
-  raise OptionParser::MissingArgument if arguments[:name].nil?
-  raise OptionParser::MissingArgument if arguments[:dns].nil?
+  
+  # Validate:
+  raise OptionParser::MissingArgument if arguments[:action].nil?
+  if arguments[:action] == :add
+    raise OptionParser::MissingArgument if arguments[:name].nil?
+  end
 
   arguments
 end
 
+#
+# Loads the config.yaml and returns the contents as hash
+#
+def get_configuration()
+  YAML.load_file("config.yml")
+end
+
+
+arguments = parse_arguments
+config = get_configuration
+
+
+puts config["apache"]["available_sites"]
 
 
 #
