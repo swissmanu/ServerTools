90c6b2a32b5b57d1340ad67542284065864389ce
Manuel Alabor <msites@msites.net>
2011-10-19 08:36:17 +0200

    implemented document root creation

--- VirtualHost.rb
+++ VirtualHost.rb
@@ -44,28 +44,47 @@ def get_configuration()
 end
 
 #
+# Creates the complete path passed and puts a message to the console.
+#
+def create_directory(path)
+  puts "Create directory\t#{path}"
+  FileUtils.mkpath(path)
+end
+
+#
 # Adds a VirtualHost to Apache
 # Actions:
 #  * Add config in available_sites
 #  * Add symlink in enabled_sites
 #  * Add virtual host to virtual host configuration
 #
-def add_virtualhost(arguments, config)
-  puts "hi"
+def add_virtualhost(virtualhost_name, config)
+  puts "Add new VirtualHost\t#{virtualhost_name}"
   
-  #
-  # 
-  #
-  #if !File.directory?('test')
-  #  FileUtils.mkdir('test')
-  #end
+  document_root = config["apache"]["documentroots"] + "/#{virtualhost_name}"
+  available_site = config["apache"]["available_sites"] + "/#{virtualhost_name}"
+  enabled_site = config["apache"]["enabled_sites"] + "/#{virtualhost_name}"
+  htdocs_path = "#{document_root}/htdocs"
+  logs_path = "#{document_root}/logs"
+  
+  if File.directory?(document_root)
+    puts "It seems that the VirtualHost \"#{virtualhost_name}\" already exists. Exiting."
+    exit
+  end
+  
+  # Add document root with its subfolders:
+  create_directory(htdocs_path)
+  create_directory(logs_path)
 end
 
 
 #
 # Run the actual script ;-)
 #
+puts "VirtualHost Management"
+puts "======================"
+
 arguments = parse_arguments
 config = get_configuration
 
-add_virtualhost(arguments, config) if arguments[:action] == :add
+add_virtualhost(arguments[:name], config) if arguments[:action] == :add
