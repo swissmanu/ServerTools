a7a84a509ace498ec5e775a0dab6bd5402a30ab1
Manuel Alabor <msites@msites.net>
2011-10-21 15:48:12 +0200

    added apache restart command to enable & disable

--- lib/servertools/apache/server.rb
+++ lib/servertools/apache/server.rb
@@ -8,6 +8,13 @@ module ServerTools
         ServerTools::Logger.message("Reload Apache configuration")
         `/etc/init.d/apache2 reload`
       end
+      
+      ##
+      # Restarts Apache
+      def self.restart!
+        ServerTools::Logger.message("Restart Apache")
+        `/etc/init.d/apache2 restart`
+      end
     end
     
   end
