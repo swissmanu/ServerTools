eb75d2f5ccdc36d8c6a5df9068768be837383074
Manuel Alabor <msites@msites.net>
2011-10-21 08:40:04 +0200

    removed old method from ServerTools::Base

--- lib/servertools/base.rb
+++ lib/servertools/base.rb
@@ -9,22 +9,6 @@ module ServerTools
   end
 
   #
-  # Picks a file and replaces contained placeholders with the values from the
-  # hash placeholders_with_values.
-  # The resulting content gets then written to target.
-  #
-  def customize_file(source, target, placeholders_with_values)
-    puts "Customize file\t\t#{source} -> #{target}"
-
-    content = File.read(source)
-    placeholders_with_values.keys.each do |placeholder|
-      content = content.gsub(/\{#{placeholder}\}/, placeholders_with_values[placeholder])
-    end
-
-    File.open(target, "w") do |f| f.puts content end
-  end
-
-  #
   # Deletes a file and puts a message to the console.
   #
   def delete_file(file)
